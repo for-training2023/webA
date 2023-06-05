fb5ac137d790176a23ea58a496c8590b82b21b82
s-nanami <nanami.shimizu@excd.jp>
2023-06-05 17:54:40 +0900

    新規S00009のサーブレット、jspです。

---
+++ src/main/java/jp/excd/servlet/S00009.java
@@ -0,0 +1,73 @@
+package jp.excd.servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class S00009 extends HttpServlet {
+
+	public void doGet(
+			HttpServletRequest request,
+			HttpServletResponse response)
+			throws IOException, ServletException {
+		
+		getServletConfig().getServletContext().getRequestDispatcher("/ja/S00009.jsp").forward(request, response);
+	}
+
+	public void doPost(
+			HttpServletRequest request,
+			HttpServletResponse response)
+			throws IOException, ServletException {
+
+		Connection con = null;
+		request.setCharacterEncoding("UTF-8");
+
+		String dbName = "meloko";
+		String userName = "meloko";
+		String password = "exceed";
+		String timeZone = "JST";
+
+		try {
+			// (1)DB接続（コネクションの確立）
+			con = MySQLSetting.getConnection(dbName, userName, password, timeZone);
+			
+			// (2)内部メソッド呼び出し
+			this.mainProcessForSearch(request, response, con);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			getServletConfig().getServletContext().getRequestDispatcher("/jsp/500.jsp").forward(request, response);
+
+		} finally {
+			try {
+				if (con != null) {
+					
+					// (3)接続したコネクションの切断を行う。
+					con.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+				getServletConfig().getServletContext().getRequestDispatcher("/jsp/500.jsp").forward(request, response);
+			}
+
+		}
+	}
+	
+	private void mainProcessForSearch(HttpServletRequest request, HttpServletResponse response, Connection con)
+			throws IOException, Exception {
+		
+		// 接続URL受け取り
+		String URL = request.getRequestURI();
+
+		// (1) 接続URLが「/ja/S00009」以外の場合は、404.jspへフォワーディングする。
+		if ("/web/ja/S00009".equals(URL)) {
+		} else {
+			getServletConfig().getServletContext().getRequestDispatcher("/jsp/404.jsp").forward(request, response);
+		}
+	}
+}
---
+++ src/main/webapp/ja/S00009.jsp
@@ -0,0 +1,67 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta name="viewport"
+	content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta name="keywords"
+	content="作曲アプリ,Meloko,楽譜,iPhone,iPad,iOS,MIDI,メロコ,作詞,作曲,コミュニティー,スマホ">
+<meta name="description"
+	content="「メロコ」はiPhone,iPadで動作する作曲アプリです。思いついたメロディーをどんどん曲として保存していきましょう。">
+<title>アプリダウンロード</title>
+<link rel="stylesheet" href="../css/main.css">
+<script src="../js/jquery-3.3.0.min.js"></script>
+<script src="../js/util.js"></script>
+
+<!-- 画像の圧縮表示設定 -->
+<style>
+
+</style>
+</head>
+<body>
+	<!-- メニューのキャンセルレイヤの起点 -->
+	<div id="layer_marker"></div>
+	
+	<div class="wrapper">
+		<!-- タイトルバー -->
+		<div class="title_bar">
+		
+			<p class="sub_title">アプリダウンロード</p>
+			
+			<a href="#" id="menu_open"> <img alt="メニュー"
+				src="../images/menu.png" class="menu-icon">
+			</a>
+			<!-- メニューの起点 -->
+			<div id="menu_marker"></div>
+		</div>
+		<p>当コミュニティーの参加には、専用アプリ「メロコ」が必要です。
+			専用アプリ「メロコ」をお使いになれば、曲の視聴、コメントなど行うことができます。</p>
+		<div class="img">
+			<figure class="pic1">
+				<a
+					href="https://apps.apple.com/jp/app/%E3%83%A1%E3%83%AD%E3%82%B3/id1440134774">
+					<img alt="melokoicon" class="melokoicon"
+					src="../images/melokoIcon.png" width="100" height="100">
+				</a>
+			</figure>
+			<br> <a
+				href="https://apps.apple.com/jp/app/%E3%83%A1%E3%83%AD%E3%82%B3/id1440134774">
+				<img alt="melokoicon" class="app" src="../images/applestore.png">
+			</a>
+		</div>
+		<!-- ページトップへjavaScript -->
+		<div id="pagetop" hidden>
+			<img alt="ページトップ" src="../images/pagetop.png">
+		</div>
+		<!-- フッター -->
+		<footer>
+			Copyright <a href="https://www.excd.jp/">© EXCEED Co., Ltd.</a> All
+			Rights Reserved.
+		</footer>
+
+	</div>
+</body>
+</html>
