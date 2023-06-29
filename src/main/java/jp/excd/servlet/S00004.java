c144048ee6e75de5cd5cfd8be582651744fe7afe
MasatoYokoi <masato.yokoi@excd.jp>
2023-06-29 09:37:18 +0900

    S00004(エポック秒用テストコード削除済み)

--- src/main/java/jp/excd/servlet/S00004.java
+++ src/main/java/jp/excd/servlet/S00004.java
@@ -3,6 +3,7 @@ package jp.excd.servlet;
 import java.io.IOException;
 import java.net.URLDecoder;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -288,15 +289,11 @@ public class S00004 extends HttpServlet {
 		double d_releaseDay = 0;
 
 		//現在のエポック秒を取得
-//		Date date = new Date(0);
-//		Double nowEpoch = (double) date.getTime();
+		Date date = new Date(0);
+		Double nowEpoch = (double) date.getTime();
 
 		//差分を算出
-//		Double diff = (nowEpoch - release_datetime) * 1000;
-		
-		//テスト用コード
-		Double diff = (1687486541 - release_datetime) * 1000;
-
+		Double diff = (nowEpoch - release_datetime) * 1000;
 
 		//小数点以下を切り捨てる処理
 		NumberFormat numberFormat = NumberFormat.getInstance();
